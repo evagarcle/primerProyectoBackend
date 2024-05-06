8e77b2dbd5ac88218bd4e4a9569eb4453beb3e4c
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-06 13:30:13 +0200

    validaciones campos usuario

--- models/user.js
+++ models/user.js
@@ -13,11 +13,47 @@ module.exports = (sequelize, DataTypes) => {
       User.hasMany(models.Order)
     }
   }
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
+  User.init(
+    {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg:"Por favor introduce tu nombre"
+        },
+      } ,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg:"Por favor introduce tu apellido"
+        },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: "Por favor introduce tu correo"
+        },
+        isEmail: {
+          msg: "Por favor introduce un correo válido"
+        },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg:"Por favor introduce tu contraseña"
+        },
+      },
+    }
   }, {
     sequelize,
     modelName: 'User',
