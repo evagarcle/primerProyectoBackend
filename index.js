cdcf2668c7e04a6b231be0cbaf79c93d3807ce40
Eva Garcia <evagarciaclemente96@gmail.com>
2024-05-27 18:09:52 +0200

    cors instalado

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const app = express()
 const PORT = 3002
-
+const cors = require("cors")
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/categories", require("./routes/categories")) /** PREFIJO */
 app.use("/funkos", require("./routes/funkos")) /** PREFIJO */
